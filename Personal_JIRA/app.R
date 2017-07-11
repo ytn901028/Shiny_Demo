f27a75423a7ce515787497f3d85cb2ef3bee44c3
adc-myang <tyang@auction.com>
2017-07-11 16:45:17 -0700

    Changes on 071117

--- Personal_JIRA/app.R
+++ Personal_JIRA/app.R
@@ -11,16 +11,21 @@
 Sys.setenv(JAVA_HOME='C:\\Program Files\\Java\\jre1.8.0_131')
 #Sys.setenv(JAVA_HOME='C:\\Program Files (x86)\\Java\\jre1.8.0_131')
 
-library(shiny)
-library(ReporteRs)
+
+
+library(shinyjs)
 
 
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-  navlistPanel(
-    tabPanel("Create Ticket",
+if (interactive()) {
+  library(shiny)
+  library(ReporteRs)
+
+  ui <- fluidPage(
+    useShinyjs(),
+    navlistPanel(
+      tabPanel("Create Ticket",
              textInput('summary',label='Summary'),
              selectInput("priority",
                          "Priority",
@@ -32,29 +37,31 @@ ui <- fluidPage(
              #checkboxInput("confirm_creation", "Confirm to create ticket"),
              actionButton('create','Create'),
              actionButton('clear', 'Clear')
-    ),
-    tabPanel("Open Tickets",
+      ),
+      tabPanel("Open_tickets",
              tableOutput("open_tickets"),
              actionButton('close', 'Close Tickets')
-    ),
-    tabPanel("Closed Tickets",
+             
+      ),
+      tabPanel("Closed Tickets",
              
              dataTableOutput("closed_tickets")
              #textOutput("closed_tickets")
+      )
     )
   )
-)
 
 # Define server logic required to draw a histogram
-server <- function(input, output, session) {
-   summary <- reactive({input$summary})
-   description <- reactive({input$description})
-   point <- reactive({input$point})
-   ticket_number <- reactive({input$create})
-   df <- reactiveValues(open_data=NULL, closed_data = NULL)
+  server <- function(input, output, session) {
+     summary <- reactive({input$summary})
+     description <- reactive({input$description})
+     point <- reactive({input$point})
+     ticket_number <- reactive({input$create})
+     status <- reactive({input$status})
+     df <- reactiveValues(open_data=NULL, closed_data = NULL)
    
    
-   open_list <- eventReactive(input$create,
+     open_list <- eventReactive(input$create,
                                {   #ticket_number <-  paste0('<label><input type="checkbox" id="check"', 
                                   #                          ticket_number(), 
                                   #                          '"> <span>', 
@@ -67,77 +74,58 @@ server <- function(input, output, session) {
                                                          stringsAsFactors = FALSE)
                                    setNames(df$open_data, c("Ticket_Number","Summary","Description","Priority","Point", "Check"))
                                    
-                                   
-                                   
                               
                                })
    
    
-   closed_list <- eventReactive(input$close,
+     closed_list <- eventReactive(input$close,
                               { 
-                                #res <- unlist(lapply(1:nrow(open_list()),
-                                #                     function(i) input[[paste0("check", open_list()[i,1])]]))
-                                
-                                
-                                res <- unlist(lapply(1:nrow(df$open_data),
-                                                                    function(i) input[[paste0("check", df$open_data[i,1])]]))
+                                res <- unlist(lapply(1:nrow(open_list()),
+                                                                    function(i) input[[paste0("check", open_list()[i,1])]]))
 
-                                
-                                
                                 closed_ticket <- open_list()[res,]
                                 closed_ticket <- subset(closed_ticket, select = seq(1,5))
                                 df$closed_data <- data.frame(rbind(df$closed_data, closed_ticket),
                                                            stringsAsFactors = FALSE)
-<<<<<<< HEAD
-                                closed_df$data <- unique(closed_df$data)
-                                setNames(closed_df$data, c("Ticket_Number","Summary","Description","Priority","Point"))
-            
-=======
                                 setNames(df$closed_data, c("Ticket_Number","Summary","Description","Priority","Point"))
-                                
-                                
-                                
->>>>>>> d8183990f39c03eb66e6576338133a0cc6a5306c
+
+               
                               })
    
 
-
+     #onevent("mouseleave", "close", reset("status"))
 
    
    
-   output$open_tickets <- renderFlexTable({
-      if(is.null(open_list()))
-       return(NULL)
-     temp <- open_list()
-     res <- unlist(lapply(1:nrow(df$open_data),
-                         function(i) input[[paste0("check", df$open_data[i,1])]]))
-     if (any(res) & input$close > 0)
-         temp <- temp[!res,]
-     clean_ticket <- vanilla.table(temp)
-     clean_ticket[, "Check", to = "header"] <- parLeft()
-     clean_ticket[, "Check"] <- parCenter()
-     clean_ticket[, "Ticket_Number"] <- parCenter()
-     clean_ticket[, "Summary"] <- parCenter()
-     clean_ticket[, "Description"] <- parCenter()
-     clean_ticket[, "Priority"] <- parCenter()
-     clean_ticket[, "Point"] <- parCenter()
-     return(clean_ticket)
+     output$open_tickets <- renderFlexTable({
+       if(is.null(open_list()))
+         return(NULL)
+       temp <- open_list()
+       clean_ticket <- vanilla.table(temp)
+       clean_ticket[, "Check", to = "header"] <- parLeft()
+       clean_ticket[, "Check"] <- parCenter()
+       clean_ticket[, "Ticket_Number"] <- parCenter()
+       clean_ticket[, "Summary"] <- parCenter()
+       clean_ticket[, "Description"] <- parCenter()
+       clean_ticket[, "Priority"] <- parCenter()
+       clean_ticket[, "Point"] <- parCenter()
+       return(clean_ticket)
      
-   }
-   )
+     }
+     )
      
 
-   output$closed_tickets <- renderDataTable({
-      if(is.null(closed_list()))
-        return(NULL)
-      unique(closed_list())
+     output$closed_tickets <- renderDataTable({
+        if(is.null(closed_list()))
+          return(NULL)
+        unique(closed_list())
      
-   })
+     })
   
   
-   session$onSessionEnded(stopApp)
+     session$onSessionEnded(stopApp)
+  }
 }
-
 # Run the application 
 shinyApp(ui = ui, server = server)
 
