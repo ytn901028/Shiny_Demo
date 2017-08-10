1f638210dd65da392c4bbb6777255142ad4c1ddd
Michael Yang <tyang@nrpi.local>
2017-08-09 19:14:20 -0700

    Model modification

--- MTV/MTV_Train.R
+++ MTV/MTV_Train.R
@@ -1,7 +1,7 @@
 library(glmnet)
 
 data<- read.csv('MTV_Data.csv')
-rundate <- as.Date(format(Sys.time(), "%Y-%m-%d"))
+rundate <- as.Date(format(Sys.time(), "%Y-%m-%d")) + 3
 
 data$Age <- 2017 - data$Year.Built
 data$Lot.Size[which(is.na(data$Lot.Size))] <- median(data$Lot.Size,na.rm=T)
@@ -9,12 +9,17 @@ data$Lot.Size[which(is.na(data$Lot.Size))] <- median(data$Lot.Size,na.rm=T)
 data$Last.Sale.Date <- as.Date(data$Last.Sale.Date,format='%m/%d/%Y')
 data$Days_Diff <- rundate - data$Last.Sale.Date
 
-data <- subset(data,Zip==94043)
+
+
+data <- subset(data,Zip==94043 & IsCondo==0)
+
 data <- data[,6:ncol(data)]
 
 
 
-train <- data[1:(nrow(data)-2),]
+train <- data[1:(nrow(data)-8),]
+
+validation <- data[(nrow(data)-7):(nrow(data)-2),]
 
 test <- data[(nrow(data)-1):nrow(data),]
 
@@ -34,7 +39,6 @@ formula = {as.formula(Last.Sale.Price
                       +Views
                       +Favorites
                       +Cross_Outs
-                      +IsCondo
 )}
 
 x <- model.matrix(object = formula, data=train)
@@ -44,11 +48,16 @@ y <- train$Last.Sale.Price
 
 
 
-model <- lm(formula=formula,data=train)
-#model2 <- glmnet(x,y,alpha=0.5,nlambda=10,family='gaussian',lambda.min.ratio = 0.0001)
-model2 <- glmnet(x,y,alpha=0.5,lambda=90605.2,family='gaussian')
+#model <- lm(formula=formula,data=train)
+#predict(model,test)
 
-predict(model,test)
+#model2 <- glmnet(x,y,alpha=0.05,family='gaussian',lambda.min.ratio = 0.02015)
+model2 <- glmnet(x,y,alpha=0.05,lambda=148896,family='gaussian',lambda.min.ratio = 0.02015)
+
+
+new_validation <- validation
+validx <- model.matrix(object=formula,data=new_validation)
+predict(model2, newx=validx)
 
 new_test <- test
 new_test$Last.Sale.Price <- 0
@@ -56,9 +65,7 @@ predx <- model.matrix(object=formula,data=new_test)
 predict(model2, newx=predx)
 
 model$coefficients
-model2$lambda
-
-
-
+mean(model2$lambda[seq(60,66)])
+model2$lambda[65]
 
 
